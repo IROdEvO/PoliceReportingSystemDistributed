bbc9a3a3dd738be8d1d72bb223832f4121976273
Ayesh Dulanja <ayeshdulanja@gmail.com>
2020-04-21 09:59:25 +0530

    Web Application

---
+++ PRS/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<br>
+<center>
+	<div>
+		<table>
+			<tr>
+			    <th><a href="adminLogin.php"><img src="images/admin.png"><h3>Login as a Admin</h3></th></a>
+			    <th><a href="policeLogin.php"><img src="images/police.png"><h3>Login as a Officer</h3></a></th>
+			    <th><a href="driverLogin.php"><img src="images/driver.png"><h3>Login as a Driver</h3></a></th>
+  			</tr>
+		</table>
+	</div>
+</center>
+
+</body>
+</html>
+
+<?php  
+
+
+
+
+
+
+
+?>
